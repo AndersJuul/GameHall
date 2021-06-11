83be9183d350d75a1ab5c9ef6c8cfaa98cff88ce
DESKTOP-HQ6G1IM\Anders <andersjuulsfirma@gmail.com>
2021-06-11 12:35:52 +0200

    no message

---
+++ GameHall.SharedKernel.Infrastructure/DataStorage/EventStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using GameHall.SharedKernel.Core;
+
+namespace GameHall.SharedKernel.Infrastructure.DataStorage
+{
+    public class EventStore:IEventStore
+    {
+        public static string StreamId(BaseEntity baseEntity)
+        {
+            return $"{baseEntity.GetType().Name}-{baseEntity.Id}";
+        }
+        public static string StreamId(Guid id)
+        {
+            return $"Account-{id}";
+        }
+
+        public void SaveEvents(Guid aggregateId, IEnumerable<BaseDomainEvent> events, int expectedVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BaseDomainEvent> GetEventsForAggregate(Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameHall.SharedKernel.Infrastructure/DataStorage/IEventStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using GameHall.SharedKernel.Core;
+
+namespace GameHall.SharedKernel.Infrastructure.DataStorage
+{
+    public interface IEventStore
+    {
+        void SaveEvents(Guid aggregateId, IEnumerable<BaseDomainEvent> events, int expectedVersion);
+        List<BaseDomainEvent> GetEventsForAggregate(Guid aggregateId);
+    }
+}
